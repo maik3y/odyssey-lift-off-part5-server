9127bcadedf21fe9112d6b39ac5e6f48c3f88a8b
Maikey <maikey.sip@proudnerds.com>
2022-03-21 14:32:35 +0100

    removed module change from shema

--- src/schema.js
+++ src/schema.js
@@ -63,8 +63,6 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
